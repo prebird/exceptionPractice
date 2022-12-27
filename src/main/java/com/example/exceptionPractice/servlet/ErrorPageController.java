1d36f81559e96e55f6f79c5ad8d0b57d8d0f44c4
yonggyujeong <yonggyu.jeong10@gmail.com>
2022-12-27 22:05:11 +0900

    api 서버일 경우 responseEntity에 에러 담아서 반환

--- src/main/java/com/example/exceptionPractice/servlet/ErrorPageController.java
+++ src/main/java/com/example/exceptionPractice/servlet/ErrorPageController.java
@@ -1,13 +1,20 @@
 package com.example.exceptionPractice.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static javax.servlet.RequestDispatcher.ERROR_EXCEPTION;
+import static javax.servlet.RequestDispatcher.ERROR_STATUS_CODE;
 
 @Slf4j
 @Controller
@@ -18,9 +25,18 @@ public class ErrorPageController {
         return "error-page/500";
     }
 
-//    // api header의 accept type으로 분기
-//    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public
+    // api header의 accept type으로 분기
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500Api(
+            HttpServletRequest request, HttpServletResponse response) {
+        HashMap<String, Object> error = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION); // RequestDispacher에 정의됨
+        error.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        error.put("message", ex.getMessage());
+
+        Integer statusCode = (Integer) request.getAttribute(ERROR_STATUS_CODE);
+        return new ResponseEntity<>(error, HttpStatus.valueOf(statusCode));
+    }
 
 
 }
